97aceb9c9b34232a6d3e0355a1b525c485ec1112
Shriyash Dongarkar <shriyashdongarkar@gmail.com>
2025-05-20 14:56:30 +0530

    Standalone Collections

--- Dependency_Injection/src/main/java/org/basics/App.java
+++ Dependency_Injection/src/main/java/org/basics/App.java
@@ -11,16 +11,16 @@ public class App {
     public static void main(String[] args) {
         AbstractApplicationContext context = new ClassPathXmlApplicationContext("basicsConfig.xml");
 
-        Temp temp1 = (Temp) context.getBean("temp1");
+        Temp temp1 = (Temp) context.getBean("usingCallbacks1");
         System.out.println(temp1);
         System.out.println(temp1.getTempValues());
 
-        Temp temp2 = (Temp) context.getBean("temp2");
+        Temp temp2 = (Temp) context.getBean("usingCallbacks2");
         System.out.println(temp2);
         System.out.println(temp2.getTempValues());
 
-        AbstractApplicationContext abstractContext = context;
-        Temp destroyTemp = (Temp) abstractContext.getBean("temp1");
-        abstractContext.registerShutdownHook();
+        Temp temp3 = (Temp) context.getBean("temp1");
+        System.out.println(temp3);
+        System.out.println(temp3.getTempValues());
     }
 }
